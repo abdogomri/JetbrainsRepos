9af47a368f74da16702edde086ac751f40391f12
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2022-11-21 22:41:00 +0100

    add Appmodule

---
+++ app/src/main/java/com/ob/jetbrainsrepos/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.ob.jetbrainsrepos.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class, FragmentComponent::class)
+object AppModule {
+
+}
