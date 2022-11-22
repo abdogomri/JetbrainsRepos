a2432029808ac19a68d7bd391d7b7d77e8af9c28
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2022-11-22 01:46:05 +0100

    Modify Navgraph to handle data on action (show details) , add parcelable to RepoInfoDetails so we can passe it on navigation action

--- app/src/main/java/com/ob/jetbrainsrepos/features/repos_feature/domain/model/RepoInfoDetails.kt
+++ app/src/main/java/com/ob/jetbrainsrepos/features/repos_feature/domain/model/RepoInfoDetails.kt
@@ -1,6 +1,11 @@
 package com.ob.jetbrainsrepos.features.repos_feature.domain.model
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class RepoInfoDetails(
-    var fullName: String? = null,
-    var description: String? = null
-)
+    @SerializedName("fullName") var fullName: String? = null,
+    @SerializedName("description") var description: String? = null
+): Parcelable
