d6e34b962d1c9df9998e4949cb048d2e08a24c7c
Abderrahim El Gomri <abdo.elgomri@gmail.com>
2022-11-22 02:00:22 +0100

    add DetailsFragment

--- app/src/main/java/com/ob/jetbrainsrepos/features/repos_feature/presentation/details/RepoDetailsFragment.kt
+++ app/src/main/java/com/ob/jetbrainsrepos/features/repos_feature/presentation/details/RepoDetailsFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.ob.jetbrainsrepos.R
-import com.ob.jetbrainsrepos.databinding.FragmentSecondBinding
+import androidx.navigation.fragment.navArgs
+import com.ob.jetbrainsrepos.databinding.RepoDetailsFragmentBinding
+import com.ob.jetbrainsrepos.features.repos_feature.domain.model.RepoInfoDetails
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -16,25 +16,29 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class RepoDetailsFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: RepoDetailsFragmentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var repoInfoDetails: RepoInfoDetails
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val args = navArgs<RepoDetailsFragmentArgs>()
+        repoInfoDetails = args.value.repoDetails
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = RepoDetailsFragmentBinding.inflate(inflater, container, false)
+        binding.fullNameTxtv.text = repoInfoDetails.fullName
+        binding.descriptionTxtv.text = repoInfoDetails.description
         return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
 
     }
 
